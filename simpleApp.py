ddad0408b5154ac5be01181bf4ea5a2dee21b8bc
Marvincorp <marvinrinaldo@gmail.com>
2014-03-28 14:59:07 +0100

    Never mind

--- simpleApp.py
+++ simpleApp.py
@@ -17,8 +17,9 @@ OAUTH_TOKEN = '2322817648-npNVa1PhLaspojJ4z04qkfnCKZPtvFR5LqPX8Fa'
 OAUTH_TOKEN_SECRET = 'DmuYAL9RivUMZmCa7zlOD4tG3Nx6G4qj24pLT9MI8oUjl'
 auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET,CONSUMER_KEY, CONSUMER_SECRET)
 twitter_api = twitter.Twitter(auth=auth) 
-
 relatedKey = 'sampleapikey'
+
+
 
 @app.route("/")
 def first_page():
@@ -32,27 +33,35 @@ def search():
 	count = 5
 	search_results = twitter_api.search.tweets(q=q, count=count, lang="en")
 	statuses = search_results['statuses'] 
-	status_texts = [ status['text']
-		for status in statuses ]	
+	status_texts = [ status['text'] for status in statuses ]	
+	
 	locations = []
 	for status in statuses:
 		if 'coordinate' in status:
 			locations.append(status['coordinate'])
 		else:
 			locations.append('No location')
-	p = {'key': relatedKey, 'base': q}
-	req = requests.get('http://www.veryrelated.com/related-api-v1.php', params = p)
+	
 	synonyms = []
 	for syn in wn.synsets(q):
 		for lemma in syn.lemma_names:
 			synonyms.append(lemma)
 	synset = set(synonyms)
-	if req.status_code == requests.codes.ok:
-		related = getRelated(req.content)	
+	
+	# hypernyms = []
+	# for hyp in wn.synset(q).hypernyms():
+		# for lemma in hyp.lemma_names:
+			# hypernyms.append(lemma)
+	# hypset = set(hypernyms)
+	
+	related = getRelated(q)	
 	return json.dumps([search_results, analyse(search_results),related, list(synset), locations])
 
-def getRelated(xml):
-	parsed = xmltodict.parse(xml)
+def getRelated(q):
+	p = {'key': relatedKey, 'base': q}
+	req = requests.get('http://www.veryrelated.com/related-api-v1.php', params = p)
+	if req.status_code == requests.codes.ok:
+		parsed = xmltodict.parse(req.content)
 	if 'Error' in parsed:
 		return {'No very related words': 0}
 	related = {rel['Text']: rel['HowRelated'] for rel in parsed['ResultSet']['Result']} 
@@ -72,7 +81,7 @@ def analyse(search_results):
 		for mention in tweet['entities']['user_mentions']:
 			screenNames.append(mention['screen_name'])
 	words = analyseText(texts)
-	return {"hashtags": sorted(Counter(hashtags).items(), key=lambda t: t[1], reverse=True), "screenNames": sorted(Counter(screenNames).items(), key=lambda t: t[1], reverse=True), "words": sorted(Counter(words).items(), key=lambda t: t[1], reverse=True)}
+	return {"hashtags": sorted(Counter(hashtags).items(), key=lambda t: t[1], reverse=True)[:5], "screenNames": sorted(Counter(screenNames).items(), key=lambda t: t[1], reverse=True)[:5], "words": sorted(Counter(words).items(), key=lambda t: t[1], reverse=True)[:5]}
 
 def analyseText(texts):
 	prefixes = ('http:','#','@','RT')
